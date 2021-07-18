21f0272b7411ecbd06284f3c03b333bd625d3906
codebysaadbouh <74369527+codebysaadbouh@users.noreply.github.com>
2021-07-18 20:14:32 +0200

    Ajoutez des fichiers projet.

---
+++ maths_game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace maths_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
