ad57cde06fdbffc62f2f850a057d1233fb0721f3
Cheikh Saad Bouh SOW <saadbouh.code@gmail.com>
2021-08-02 22:22:25 +0200

    1- Définir min et max 2- nombre aléatoire a et b 3- Calculer a et b 4- retourner true / false et afficher bonne ou mauvaise réponse

--- maths_game/Program.cs
+++ maths_game/Program.cs
@@ -4,9 +4,68 @@ namespace maths_game
 {
     class Program
     {
+        
+        static bool poserQuestion(int min_number, int max_number)
+        {
+            Random rand = new Random();
+            int a = rand.Next(min_number, max_number++);
+            int b = rand.Next(min_number, max_number++);
+            int result = a + b; 
+
+
+            int response_int = 0;
+            // bool response_bool; 
+            while (true)
+            {
+                Console.Write($"{a} + {b} = ");
+                string response_str = Console.ReadLine();
+
+                try
+                {
+                    response_int = int.Parse(response_str);
+                    // response_bool = response_int == result;
+                    if (response_int == result)
+                    {
+                        // Console.WriteLine("Bonne réponse");
+                        return true; 
+                    }
+                    /**
+                         else
+                        {
+                            Console.WriteLine("Mauvaise réponse"); 
+                        }
+                     */
+
+                    return false; 
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR : vous devez rentrer un nombre !");
+                }
+            }
+
+            // Console.WriteLine(response_bool); 
+            // return response_bool;
+
+        }
+       
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int MIN_NUMBER = 1;
+            const int MAX_NUMBER = 30;
+
+            bool reponse = poserQuestion(MIN_NUMBER, MAX_NUMBER);
+
+            if (reponse)
+            {
+                Console.WriteLine("Bonne réponse");
+            }
+            else
+            {
+                Console.WriteLine("Mauvaise réponse");
+            }
+
         }
     }
 }
